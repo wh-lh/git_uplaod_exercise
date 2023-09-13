78019e3261b198e5f4dceca9f213086738e1778c
James2 <hangwang0404@163.com>
2023-09-13 15:20:18 +0800

    Merge branch 'a'

--- sdfgs.java
+++ sdfgs.java
@@ -1,11 +1,10 @@
 public class sdfgs {
     public static void main(String[] args) {
         System.out.println("hhh");
-<<<<<<< HEAD
-        System.out.println("sdfsba");
-=======
         System.out.println("sdfsbd");
         System.out.println("asdadasda");
->>>>>>> a
+        System.out.println("ajiayi");
+        System.out.println("ajiaer");
+
     }
 }
